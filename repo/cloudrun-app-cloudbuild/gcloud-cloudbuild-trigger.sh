d9eb400da10a0e07f470d6c0551962cecc695699
rando <rando@cogitos.com>
2025-10-29 11:10:03 +0530

    Moved out repo

--- repo/cloudrun-app-cloudbuild/gcloud-cloudbuild-trigger.sh
+++
@@ -1,2 +0,0 @@
-# Command to run the build using cloudbuild.yaml
-gcloud builds submit --region us-central1
--- repo/cloudrun-app-cloudbuild/gcloud-permission-commands.sh
+++
@@ -1,6 +0,0 @@
-# Assign Service account user role to the service account 
-gcloud projects add-iam-policy-binding cicd-476509 --member=serviceAccount:cicd-sa@cicd-476509.iam.gserviceaccount.com --role=roles/iam.serviceAccountUser
-
-
-# Assign Cloud Run role to the service account 
-gcloud projects add-iam-policy-binding cicd-476509 --member=serviceAccount:cicd-sa@cicd-476509.iam.gserviceaccount.com --role=roles/run.admin
